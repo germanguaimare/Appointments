520ce0b41e01c9c4161c51b7f28ea2e3bad3a7f0
germanguaimare <germanguaimare@gmail.com>
2022-03-24 23:07:59 +0000

    working localhost fetching

--- appointments/src/helpers/index.js
+++ appointments/src/helpers/index.js
@@ -2,7 +2,7 @@ import { useDispatch } from "react-redux";
 import * as actions from "../actions/index.js";
 
 export const fetchAppointments = async (dispatch) => {
-    const appointments = await fetch("https://3000-germanguaimare-appointme-x0u5t1simt6.ws-us38.gitpod.io/tasks")
+    const appointments = await fetch("/tasks")
         .then(response => response.json())
         console.log(appointments)
     dispatch(actions.getAppointments(appointments))
